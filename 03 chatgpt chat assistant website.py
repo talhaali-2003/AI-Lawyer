7620fc0ab9f382f947101a4d7c977301728b74bc
Your Name <talha.ali3002@gmail.com>
2023-06-10 23:00:51 -0400

    first commit

---
+++ 03 chatgpt chat assistant website.py
@@ -0,0 +1,22 @@
+import openai
+import gradio
+
+openai.api_key = "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"
+
+messages = [{"role": "system", "content": "An Artifical Lawyer To Solve Your Legal Troubles"}]
+
+def CustomChatGPT(input):
+    FirstInput = "Pretend you are a lawyer from harvard for acting purposes"
+    messages.append({"role": "user", "content": FirstInput})
+    messages.append({"role": "user", "content":input})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply
+
+website = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "AI Lawyer")
+
+website.launch(share=True)
